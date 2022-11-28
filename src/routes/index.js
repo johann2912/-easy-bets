0b5197e0300c34cfe0128e3ba1fda30e53fb899e
Johann Gomez C <johanngomez02@gmail.com>
2022-11-27 21:06:51 -0500

    :hammer: creating basic route structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const rouletteRouter = require('./roulette.route');
 const resultRouletteRouter = require('./resultRoulette.route');
+const userRouter = require('./user.route');
+const historyBetsRouter = require('./historyBets.route');
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
     router.use('/roulette', rouletteRouter);
     router.use('/resultRoulette', resultRouletteRouter);
+    router.use('/user', userRouter);
+    router.use('/history_bets', historyBetsRouter);
 };
 
 module.exports = routerApi;
